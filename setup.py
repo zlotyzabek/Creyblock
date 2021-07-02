f19cf453efb94d8865df0f3d76f39cde1d977907
zlotyzabek <piotrjanaszak7@gmail.com>
2021-07-02 20:16:10 +0200

    exe export

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 from distutils.core import setup
-import py2exe
+import py2exe, sys, os
 
-setup(console=['main.py'])
+setup(options = {'py2exe': {'bundle_files': 1, 'compressed': True}},windows = [{'script': "main.py"}],zipfile = None,)
